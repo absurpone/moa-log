f22360a1129605b6f50bba466209fd5375f24c40
absurpone <absurppone@gmail.com>
2021-03-15 14:23:29 +0900

    バリデーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
-  
+  has_one_attached :image
+
+  validates :museum_name, presence: true
+  validates :prefecture_id, presence: true
+  validates :rating, presence: true
+  validates :image, presence: true
 end
