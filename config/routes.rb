2c79f18495a2c15d12e98c044a710ff40c44a00a
absurpone <absurppone@gmail.com>
2021-03-18 16:52:47 +0900

    ユーザーマイページ表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
   resources :posts
+  resources :users, only: :show
 
   post 'favorite/:id' => 'favorites#create', as: 'create_favorite'
   delete 'favorite/:id' => 'favorites#destroy', as: 'destroy_favorite'
