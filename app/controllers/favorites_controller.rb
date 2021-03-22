4adff8f6ba4f6074016315d7f066dbe7fa8ae736
absurpone <absurppone@gmail.com>
2021-03-22 19:21:17 +0900

    ビューの修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,6 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create, :destroy]
+  before_action :set_post, 
 
   def index
     @favorite_posts = current_user.favorite_posts
@@ -7,11 +8,17 @@ class FavoritesController < ApplicationController
 
   def create
     Favorite.create(user_id: current_user.id, post_id: params[:id])
-    redirect_to posts_path
+    redirect_back(fallback_location: post_path(@post.id)) 
   end
 
   def destroy
     Favorite.find_by(user_id: current_user.id, post_id: params[:id]).destroy
-    redirect_to posts_path
+    redirect_back(fallback_location: post_path(@post.id))
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
