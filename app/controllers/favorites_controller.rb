6fff059f3d00f7ef9cf08516316566e91eebbaa4
absurpone <absurppone@gmail.com>
2021-03-24 19:31:19 +0900

    お気に入り機能の非同期通信化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,19 +1,19 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create, :destroy]
-  before_action :set_post, 
+  before_action :set_post, only: [:create, :destroy]
 
   def index
     @favorite_posts = current_user.favorite_posts
   end
 
   def create
-    Favorite.create(user_id: current_user.id, post_id: params[:id])
-    redirect_back(fallback_location: post_path(@post.id)) 
+    Favorite.create(user_id: current_user.id, post_id: @post.id)
+    # redirect_back(fallback_location: post_path(@post.id)) 
   end
 
   def destroy
-    Favorite.find_by(user_id: current_user.id, post_id: params[:id]).destroy
-    redirect_back(fallback_location: post_path(@post.id))
+    Favorite.find_by(user_id: current_user.id, post_id: @post.id).destroy
+    # redirect_back(fallback_location: post_path(@post.id))
   end
 
   private
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,10 +5,17 @@
 
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
+require("turbolinks").start()
 require("channels")
 window.$ = window.jQuery = require('jquery');
 require('packs/raty')
 
+
+// = require rails-ujs
+// = require activestorage
+// = require jquery
+// = require turbolinks
+// = require_tree .
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
