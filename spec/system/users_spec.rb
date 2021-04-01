5eb390ebbedaddc1f0607e871e6da1403c226954
absurpone <absurppone@gmail.com>
2021-04-01 17:02:26 +0900

    ユーザー新規登録異常系結合テストコード作成

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 def visit_with_http_auth(path)
-  username = '34483'
-  password = '1212'
+  username = ENV["BASIC_AUTH_USER"]
+  password = ENV["BASIC_AUTH_PASSWORD"]
   visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
 end
 
@@ -34,6 +34,27 @@ RSpec.describe "Users", type: :system do
       # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
       expect(find('.menu-content', visible: false).text(:all)).not_to include '新規登録'
       expect(find('.menu-content', visible: false).text(:all)).not_to include 'ログイン'
+    end
+  end
+  context 'ユーザー登録できない時' do
+    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+      # Basic認証の通過
+      visit_with_http_auth root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(find('.menu-content', visible: false).text(:all)).to include '新規登録'
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'user_name', with: ''
+      fill_in 'user_email', with: ''
+      fill_in 'user_password', with: ''
+      fill_in 'user_password_confirmation', with: ''
+      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(0)
+      # 新規登録ページへ戻されることを確認する
+      expect(current_path).to eq('/users')
     end
   end
 end
