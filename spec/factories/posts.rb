88b76b3860e2d006bf591b475650bb93f6703857
absurpone <absurppone@gmail.com>
2021-04-02 16:45:21 +0900

    Update posts.rb

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     rating { 4.5 }
     exhibition_title { '国際特別展' }
     text { 'テスト用の感想です' }
-    impressive_artist { Faker::Artist.name }
+    impressive_artist { 'レオナルド・ダ・ヴィンチ' }
     impressive_work { 'テスト作品' }
     association :user
 
